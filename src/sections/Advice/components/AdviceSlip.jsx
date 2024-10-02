f1960421fe6e5860441a900f58a6ded022840203
George <george.alexander.saav@gmail.com>
2024-10-02 12:58:52 +0200

    Implemented button to fetch new advice, alongside useEffect fetch on render

--- src/sections/Advice/components/AdviceSlip.jsx
+++ src/sections/Advice/components/AdviceSlip.jsx
@@ -4,19 +4,24 @@ function AdviceSlip() {
     const url = "https://api.adviceslip.com/advice"
     const [data, setData] = useState([]);
 
-    useEffect(() => {
+    const advice = () => {
         const fetchData = async () => {
             const response = await fetch(url);
             const jsonData = await response.json();
             setData(jsonData.slip);
         };
         fetchData();
+    }
+    useEffect(() => {
+        advice();
     }, []);
-
+// Making a function of what originally just was the useEffect.
+// Function is triggered by button. Useeffect with emtpy argument array also triggers it upon render.
     return (
         <>
             <h3>Some Advice</h3>
             <p>{data.advice}</p>
+            <button onClick={advice}>Get More Advice</button><button>Save to Favourites</button>
         </>
     )
 }
